65543aa773c49f99735e0e9c3cccc96c5d84ce88
mario-cartagena <jhonmariocartagena@gmail.com>
2023-06-21 01:15:50 -0500

    component dish

--- src/pages/Dish.jsx
+++ src/pages/Dish.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import IconTime from '../assets/icons/time.png';
 import '../components/dish/styleDish.scss';
+import Checkbox from '@mui/material/Checkbox';
 
 const Dish = () => {
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
   return (
     <div className='dish'>
       <div className="dish__top">
@@ -22,6 +24,52 @@ const Dish = () => {
         </div>
         <span className="dish__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</span>
       </div>
+      <div className="dish__aditional">
+        <span className="aditional__title">Additional Ingredients</span>
+        <div className="dish__ingredients">
+          <div className="dish__first">
+            <Checkbox style={{padding: 0}}
+              {...label}
+              defaultChecked
+              sx={{
+                color: '#A7A7A7',
+                '&.Mui-checked': {
+                  color: '#FFE031',
+                },
+              }}
+            />
+            <span className='ingredient'>Tomatoes</span>
+            <span>+2$</span>
+          </div>
+          <div className="dish__second">
+            <Checkbox style={{padding: 0}}
+              {...label}
+              sx={{
+                color: '#A7A7A7',
+                '&.Mui-checked': {
+                  color: '#FFE031',
+                },
+              }}
+            />
+            <span className='ingredient'>Grain</span>
+            <span>+1$</span>
+          </div>
+          <div className="dish__third">
+            <Checkbox style={{padding: 0}}
+              {...label}
+              sx={{
+                color: '#A7A7A7',
+                '&.Mui-checked': {
+                  color: '#FFE031',
+                },
+              }}
+            />
+            <span className='ingredient'>Lettuce leaf</span>
+            <span>+1$</span>
+          </div>
+        </div>
+      </div>
+      div
     </div>
   )
 }
