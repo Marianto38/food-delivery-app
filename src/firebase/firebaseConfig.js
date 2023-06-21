960424088f7f3559b71074e72e1f79f98969c4e9
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-06-21 09:04:07 -0500

    edit function to persisted phone and birthday

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,3 +1,29 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { FacebookAuthProvider, getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCLs1_hIZ-adYZBonuof1vpKQM0C864d4A",
+//   authDomain: "food-delivery-app-3582e.firebaseapp.com",
+//   projectId: "food-delivery-app-3582e",
+//   storageBucket: "food-delivery-app-3582e.appspot.com",
+//   messagingSenderId: "191587341012",
+//   appId: "1:191587341012:web:2a14df1b0bfdebf8bbe141",
+//   measurementId: "G-XJWQ4BL3HM"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // Initialize Firebase Autentication and get a reference to the service.
+// export const auth = getAuth(app);
+// //console.log(auth);
+// export const dataBase = getFirestore(app);
+// export const google = new GoogleAuthProvider();
+// export const facebook = new FacebookAuthProvider();
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -21,3 +47,5 @@ export const auth = getAuth(app);
 export const dataBase = getFirestore(app);
 export const google = new GoogleAuthProvider();
 export const facebook = new FacebookAuthProvider();
+//Initialize db to save phone and birthday
+//const db = dataBase; // Asigna 'dataBase' a 'db'
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -26,6 +26,86 @@
 //     });
 //   }, [setIsLoggedIn, setCheking]);
 
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Register from "../pages/Register";
+// import PublicRouter from "./PublicRouter";
+// import PrivateRouter from "./PrivateRouter";
+// //import Layout from "../components/Layout";
+// import Login from "../pages/Login";
+// //import Home from "../pages/Home";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { auth } from "../firebase/firebaseConfig";
+// import Spinner from "react-bootstrap/Spinner";
+// import { useDispatch, useSelector } from "react-redux";
+// import { loginActionSync } from "../redux/actions/userActions";
+// import StepersLogin from '../components/stepersLogin/StepersLogin';
+// import DashboardRouter from './DashboardRouter';
+// import NotFound from '../../src/components/notFound/NotFound';
+
+// const AppRouter = () => {
+//   const [isLoggedIn, setIsLoggedIn] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   const dispatch = useDispatch();
+
+//   const { user } = useSelector((store) => store.user);
+//   console.log(user);
+
+//   useEffect(() => {
+//     onAuthStateChanged(auth, (userLogged) => {
+//       if (userLogged?.uid) {
+//         setIsLoggedIn(true);
+
+//         if (!Object.entries(user).length) {
+//           console.log("No hay info");
+//           const logged = {
+//             email: userLogged.auth.currentUser.email,
+//             fullName: userLogged.auth.currentUser.displayName,
+//             avatar: userLogged.auth.currentUser.photoURL,
+//             accessToken: userLogged.auth.currentUser.accessToken,
+//             phone: user.phone,
+//             birthday: user.birthday,
+//           };
+//           dispatch(loginActionSync(logged));
+//         }
+//         console.log(userLogged);
+//       } else {
+//         setIsLoggedIn(false);
+//       }
+
+//       setLoading(false);
+//     });
+//   }, [user, dispatch]);
+
+//   if (loading) {
+//     return <Spinner animation="grow" />;
+//   }
+
+
+//   // if (cheking) {
+//   //   return <h1>Espere....</h1>;
+//   // }
+//   return (
+//     <BrowserRouter>
+//      <Routes>
+//         <Route element={<PublicRouter isAutentication={isLoggedIn} />}>
+//           <Route path="/register" element={<Register />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/" element={<StepersLogin />} />
+//         </Route>
+//         <Route element={<PrivateRouter isAutentication={isLoggedIn} />}>
+//           <Route path="/*" element={<DashboardRouter />} />
+//           <Route path="*" element={<NotFound />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default AppRouter;
+
+
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "../pages/Register";
@@ -35,13 +115,15 @@ import PrivateRouter from "./PrivateRouter";
 import Login from "../pages/Login";
 //import Home from "../pages/Home";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/firebaseConfig";
+import { auth, dataBase } from "../firebase/firebaseConfig";
 import Spinner from "react-bootstrap/Spinner";
 import { useDispatch, useSelector } from "react-redux";
 import { loginActionSync } from "../redux/actions/userActions";
 import StepersLogin from '../components/stepersLogin/StepersLogin';
 import DashboardRouter from './DashboardRouter';
 import NotFound from '../../src/components/notFound/NotFound';
+import { doc, getDoc } from "firebase/firestore";
+
 
 const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
@@ -56,36 +138,54 @@ const AppRouter = () => {
     onAuthStateChanged(auth, (userLogged) => {
       if (userLogged?.uid) {
         setIsLoggedIn(true);
-
+  
         if (!Object.entries(user).length) {
           console.log("No hay info");
-          const logged = {
-            email: userLogged.auth.currentUser.email,
-            fullName: userLogged.auth.currentUser.displayName,
-            avatar: userLogged.auth.currentUser.photoURL,
-            accessToken: userLogged.auth.currentUser.accessToken,
-            phone: user.phone,
-            birthday: user.birthday,
-          };
-          dispatch(loginActionSync(logged));
+          const db = dataBase; // Asigna 'dataBase' a 'db'
+  
+          // Obtener referencia al documento del usuario en Firestore
+          const userDocRef = doc(db, 'users', userLogged.uid);
+  
+          // Obtener los datos del documento del usuario
+          getDoc(userDocRef)
+            .then((docSnap) => {
+              if (docSnap.exists()) {
+                const userData = docSnap.data();
+  
+                // Obtener phone y birthday del documento del usuario
+                const { phone, birthday } = userData;
+  
+                const logged = {
+                  email: userLogged.auth.currentUser.email,
+                  fullName: userLogged.auth.currentUser.displayName,
+                  avatar: userLogged.auth.currentUser.photoURL,
+                  accessToken: userLogged.auth.currentUser.accessToken,
+                  phone: phone, // Agregar el valor de phone
+                  birthday: birthday, // Agregar el valor de birthday
+                };
+                dispatch(loginActionSync(logged));
+              }
+            })
+            .catch((error) => {
+              console.error("Error al obtener los datos del usuario:", error);
+            });
         }
+  
         console.log(userLogged);
       } else {
         setIsLoggedIn(false);
       }
-
+  
       setLoading(false);
     });
   }, [user, dispatch]);
+  
 
   if (loading) {
     return <Spinner animation="grow" />;
   }
 
 
-  // if (cheking) {
-  //   return <h1>Espere....</h1>;
-  // }
   return (
     <BrowserRouter>
      <Routes>
